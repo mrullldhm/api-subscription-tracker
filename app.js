071ec59cedc1c6dbce0445da7b52af07fc930d8c
mrullldhm <mrullldhm94@gmail.com>
2025-08-26 04:21:03 +0800

    Error Handler

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ import authRouter from "./routes/auth.routes.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import connectToDatabase from "./database/mongodb.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+// Express built in middleware function
+app.use(express.json());
+app.use(express.urlencoded({extended:false}))
+app.use(cookieParser())
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/subscription", subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Subscription Tracker API");
